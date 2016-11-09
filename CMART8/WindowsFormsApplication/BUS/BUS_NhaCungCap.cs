a0068e8536705a512e786d14059de7a90fc17e9e
Luan Nguyen <Luan Nguyen>
2016-11-09 23:33:08 +0700

    UpdateNewProjectWithFunction_ManageNCC

---
+++ CMART8/WindowsFormsApplication/BUS/BUS_NhaCungCap.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication
+{
+    class BUS_NhaCungCap
+    {   CMART8Entities db;
+    public List<NHACUNGCAP> loadListNCC()
+    { 
+        db = new CMART8Entities();
+        return db.NHACUNGCAPs.ToList();
+    }
+
+    public List<NHACUNGCAP> searchListNCC(string sTmp)
+    {
+        db = new CMART8Entities();
+        return db.NHACUNGCAPs.Where(st => st.DIACHI.Contains(sTmp) || st.TENNCC.Contains(sTmp) || st.SDT.Contains(sTmp)).ToList();
+    }
+    public bool addNCC(string ten, string diachi, string sdt)
+    {
+        db = new CMART8Entities();
+        NHACUNGCAP ncc = new NHACUNGCAP();
+        try {
+            ncc.MANCC = DateTime.Now.ToString();
+            ncc.TENNCC = ten;
+            ncc.DIACHI = diachi;
+            ncc.SDT = sdt;
+            db.NHACUNGCAPs.AddObject(ncc);
+            db.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    public bool editNCC(string id, string ten, string diachi, string sdt)
+    {
+        db = new CMART8Entities();
+        NHACUNGCAP ncc = db.NHACUNGCAPs.Single(st => st.MANCC.Equals(id));
+        try
+        {
+            ncc.TENNCC = ten;
+            ncc.DIACHI = diachi;
+            ncc.SDT = sdt;
+            db.SaveChanges();
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    public bool deleteNCC(String id)
+    {   
+        db = new CMART8Entities();
+        NHACUNGCAP ncc = db.NHACUNGCAPs.Single(st => st.MANCC.Contains(id));
+        db.NHACUNGCAPs.DeleteObject(ncc);
+        db.SaveChanges();
+        return true;
+    }
+    public bool checkExist(string sTmp)
+    { 
+        db = new CMART8Entities();
+        List<NHACUNGCAP> tmp = db.NHACUNGCAPs.Where(st=> st.TENNCC.Equals(sTmp) ||st.MANCC.Equals(sTmp)).ToList();
+        if (tmp.Count != 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+    }
+}
